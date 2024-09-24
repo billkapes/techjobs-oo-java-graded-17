f8210c21e2d1d9fee1bba85231a2bf19bbd31959
billkapes <kapes15@yahoo.com>
2024-09-24 01:45:16 -0500

    moving on to task 6

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JobTest {
-    //TODO: Create your unit tests here
+
     @Test
     public  void testSettingJobId() {
         Job job1 = new Job();
